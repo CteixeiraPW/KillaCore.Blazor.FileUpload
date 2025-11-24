52ce6aa6529d1d25872b94fe6f7ccb281308420a
Carlos Teixeira <cteixeira@pw.lacounty.gov>
2025-11-24 11:06:54 -0800

    Fixing the endpoint again :(

--- KillaCore.Blazor.FileUpload/Models/FileProcessingOptions.cs
+++ KillaCore.Blazor.FileUpload/Models/FileProcessingOptions.cs
@@ -3,7 +3,7 @@
 public sealed class FileProcessingOptions
 {
     // --- Endpoints ---
-    public string UploadEndpointUrl { get; set; } = "api/upload";
+    public string UploadEndpointUrl { get; set; } = "api/uploads/temp";
 
     // ✅ SAFE: The user can ONLY add valid features here.
     // They cannot add "Uploading" or "Hashing" because the enum doesn't have them.
